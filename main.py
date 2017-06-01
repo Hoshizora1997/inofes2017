cae0c376366bc507e8eacd23aca556ea6bfef70f
hoshizora1997 <sato.daitetsu.sm@alumni.tsukuba.ac.jp>
2017-06-02 04:29:02 +0900

    Configページの追加など

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bottle import route, get, redirect, template, TEMPLATE_PATH, run
+from bottle import route, get, redirect, template, TEMPLATE_PATH, run, request
 #from __future__ import print_function
 import socket
 from contextlib import closing
@@ -17,23 +17,15 @@ def page():
 def red():
     redirect(mainURL+'page/', 301)
 
-
 @route('/', method='POST')
 def do():
     try:
         f = open('num', 'r')
-        print('ファイル読み込み完了')
-        n = int(f.read().strip())
-        print(n)
-        print('読み込み完了')
-        n += 1
-        print(n)
+        n = int(f.read().strip())+1
         f.close()
         f = open('./num','w')
-        print('ファイル書き込み準備完了')
         f.write(str(n))
         f.close()
-        print('ファイル書き換え成功\n')
     except:
         return '数値操作エラーが発生しました。操作をやり直してください'
 #    try:
@@ -46,5 +38,49 @@ def do():
 #    except:
 #        return '通信エラーが発生しました。操作をやり直してください'
     redirect(mainURL+'page/', 301)
+
+#--------------------------
+# Configページ
+#--------------------------
+
+@get('/config/')
+def config():
+    return template('config',)
+
+@route('/config/',method='POST')
+def do_config():
+    line = request.forms.get('line')
+    num = request.forms.get('num')
+    speed = request.forms.get('speed')
+
+    line = int(line)
+    num = int(num)
+    speed = int(speed)
+
+    retext = '設定の変更を完了しました。<br/>'#完了文の生成
+
+    #0は設定を現状維持
+    if(line != 0):
+        f = open('line','w')
+        f.write(str(line))
+        retext += '<br/>閾値：' + str(line)
+    else:
+        retext += '<br/>閾値：変化なし'
+    if(num != 0):
+        f = open('num','w')
+        f.write(str(num))
+        retext += '<br/>累計数：' + str(num)
+    else:
+        retext += '<br/>累計数：変化なし'
+    if(speed != 0):
+        f = open('speed','w')
+        f.write(str(speed))
+        retext += '<br/>秒速：' + str(speed)
+    else:
+        retext += '<br/>秒速：変化なし'
+
+    return (retext)
+
+
 
 run(host='0.0.0.0', port=8080, debug=True)
